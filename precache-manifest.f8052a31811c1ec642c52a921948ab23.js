59ffcb0c0081bb28575cc85c0b29c22ad058ef1c
psingh359 <singhpurva2906@gmail.com>
2022-10-14 14:37:52 -0400

    Updates

---
+++ precache-manifest.f8052a31811c1ec642c52a921948ab23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bde3dd5733a5aaffa8d5",
+    "url": "/react-gh-pages/static/css/main.d722fe26.chunk.css"
+  },
+  {
+    "revision": "bde3dd5733a5aaffa8d5",
+    "url": "/react-gh-pages/static/js/main.27e960e9.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "08537fdd83f8c4315889",
+    "url": "/react-gh-pages/static/js/2.ccf725aa.chunk.js"
+  },
+  {
+    "revision": "f4aab4b5c95ebb42bd5c574295c92705",
+    "url": "/react-gh-pages/index.html"
+  }
+];
